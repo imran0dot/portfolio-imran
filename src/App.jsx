640679dd0216411dcf3cf033ee49bfc5a24dee37
imran0dot <imran0dot@gmail.com>
2025-07-06 23:57:11 +0600

    navigation component has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <>
+    <div className="relative w-screen min-h-screen overflow-x-auto">
+    <Navbar />
     
-    </>
+    </div>
   )
 }
 
